9b3011a54b03a1c2ad92a0f73b7ef95cd5e68cb6
umutsogukpinar <usogukpinar0@gmail.com>
2025-09-03 23:01:20 +0300

    feat: update docker-compose and design other files

--- requirements/nginx/tools/installer.sh
+++ requirements/nginx/tools/installer.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+
 set -e
 
 # Generate SSL certificate if it does not exist
 if [ ! -f /etc/nginx/ssl/server.crt ] || [ ! -f /etc/nginx/ssl/server.key ]; then
-  openssl req -x509 -nodes -newkey rsa:2048 -days 365 \
-    -subj "/CN=localhost" \
-    -addext "subjectAltName=DNS:localhost,IP:127.0.0.1" \
-    -keyout /etc/nginx/ssl/server.key \
-    -out /etc/nginx/ssl/server.crt
-  chmod 600 /etc/nginx/ssl/server.key
+    openssl req -x509 -nodes -newkey rsa:2048 -days 365         \
+        -subj "/CN=localhost"                                   \
+        -addext "subjectAltName=DNS:localhost,IP:127.0.0.1"     \
+        -keyout /etc/nginx/ssl/server.key                       \
+        -out /etc/nginx/ssl/server.crt
+    chmod 600 /etc/nginx/ssl/server.key
 fi
 
 # Create runtime directory for Nginx
