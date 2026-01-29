b05ed5e861b3ac75afc57790f4c65141cbf13396
Umut Sogukpinar <usogukpinar0@gmail.com>
2026-01-29 13:28:39 +0300

    fix: update

--- srcs/requirements/mariadb/tools/installer.sh
+++ srcs/requirements/mariadb/tools/installer.sh
@@ -6,10 +6,11 @@ set -e
 DB_USER="${MYSQL_USER}"
 DB_NAME="${MYSQL_DATABASE}"
 
-DB_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
-DB_ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
+DB_PASSWORD="$(cat "$MYSQL_PASSWORD_FILE")"
+DB_ROOT_PASSWORD="$(cat "$MYSQL_ROOT_PASSWORD_FILE")"
 
 DATADIR="/var/lib/mysql"
+MYSQLD_ARGS="--console --datadir=${DATADIR} --user=mysql"
 
 echo "[INFO] MariaDB entrypoint starting..."
 
@@ -20,19 +21,13 @@ echo "[INFO] MariaDB entrypoint starting..."
 [ -z "$DB_PASSWORD" ] && echo "[ERROR] MYSQL_PASSWORD is empty" && exit 1
 [ -z "$DB_ROOT_PASSWORD" ] && echo "[ERROR] MYSQL_ROOT_PASSWORD is empty" && exit 1
 
-# ======================= Initialize DB =======================
+# ======================= Functions =======================
 
-MYSQLD_ARGS="--console --datadir=${DATADIR} --user=mysql"
+prepare_init_file()
+{
+	init_file="/tmp/init.sql"
 
-if [ ! -d "$DATADIR/mysql" ]; then
-    echo "[INFO] Database directory empty. Initializing..."
-
-    mysqld --initialize-insecure > /dev/null 2>&1
-    echo "[SUCCESS] Database initialized."
-    echo "[INFO] Configuring database and users..."
-
-    cat << EOF > /tmp/init.sql
-
+	cat << EOF > "$init_file"
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
@@ -43,21 +38,36 @@ CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 
 FLUSH PRIVILEGES;
-
 EOF
 
-    MYSQLD_ARGS="$MYSQLD_ARGS --init-file=/tmp/init.sql"
+	MYSQLD_ARGS="${MYSQLD_ARGS} --init-file=${init_file}"
+}
 
-    echo "[SUCCESS] Configuration file created."
+initialize_database()
+{
+	echo "[INFO] Database directory empty. Initializing..."
 
+	mysqld > /dev/null 2>&1
+
+	echo "[INFO] Configuring database and users..."
+	prepare_init_file
+
+	echo "[SUCCESS] Configuration file created."
+}
+
+# ======================= Initialize DB =======================
+
+if [ ! -d "$DATADIR/mysql" ]; then
+	initialize_database
 else
-    echo "[INFO] Existing database detected. Skipping initialization."
+	echo "[INFO] Existing database detected. Skipping initialization."
+	prepare_init_file
 fi
 
 # ======================= Start server =======================
 
 echo "[INFO] Cleaning up environment variables..."
-unset MYSQL_USER MYSQL_DATABASE MYSQL_PASSWORD_FILE MYSQL_ROOT_PASSWORD_FILE DB_PASSWORD DB_ROOT_PASSWORD
+unset DB_USER DB_NAME DB_PASSWORD DB_ROOT_PASSWORD
 
 echo "[INFO] Starting MariaDB server..."
 exec mysqld $MYSQLD_ARGS
